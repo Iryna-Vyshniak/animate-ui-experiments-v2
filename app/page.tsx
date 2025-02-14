43e9989bc74efa1ccc614b983e1ad7485e391619
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-14 09:12:06 +0200

    Add GalacticView component to Home for displaying planets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import GalacticView from '@/components/galactic/GalacticView';
 import StarrySpace from '@/components/space/StarrySpace';
 import ToggleAudio from '@/components/ToggleAudio';
 import planetsData from '@/data/planets.json';
 
 export default function Home() {
   const { planets } = planetsData;
-  console.log('planets : ', planets);
   return (
     <div className='relative min-h-screen w-full overflow-hidden'>
       <div className='absolute inset-0 -z-10'>
@@ -15,6 +15,7 @@ export default function Home() {
       <div className='grid place-items-center items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
         <main className='relative flex flex-col gap-8 items-center sm:items-start'>
           <ToggleAudio />
+          <GalacticView planets={planets} />
         </main>
       </div>
     </div>
