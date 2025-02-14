eff20a1f1c610121de6bd1e47ed078afac216fc2
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-14 09:26:52 +0200

    Add modal for displaying planet details with animation and navigation control

--- app/@modal/(.)planets/[name]/modal.tsx
+++ app/@modal/(.)planets/[name]/modal.tsx
@@ -1,15 +1,37 @@
 'use client';
 
 import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect } from 'react';
 
-const Modal = () => {
+import { IPlanet } from '@/types/planet';
+import { useLayoutNavigationController } from '@/hooks/useLayoutNavigationController';
+import { NAVIGATION_DURATION } from '@/constants/navigation';
+import PlanetDetail from '@/components/planet/PlanetDetail';
+
+const duration = NAVIGATION_DURATION / 1000;
+// Navigation_duration => 250 = example: 250 means that the duration of animation 250 milliseconds (MS). Division by 1000 converts it into seconds (0.25 sec).
+
+const Modal = ({ planet }: { planet: IPlanet }) => {
+  const { isOpenModal, open, setIsOpenModal } = useLayoutNavigationController();
+
+  useEffect(() => {
+    open();
+
+    return () => {
+      setIsOpenModal(false);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <AnimatePresence>
-      <div className='modal w-screen h-screen z-50 fixed top-0'>
-        <motion.div className='mx-auto max-w-5xl' transition={{ duration: 0.5 }}>
-          PLANET
-        </motion.div>
-      </div>
+      {isOpenModal && (
+        <div className='modal w-screen h-screen z-50 fixed top-0'>
+          <motion.div className='mx-auto max-w-5xl' transition={{ duration }}>
+            <PlanetDetail planet={planet} />
+          </motion.div>
+        </div>
+      )}
     </AnimatePresence>
   );
 };
