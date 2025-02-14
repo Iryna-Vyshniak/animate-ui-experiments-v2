adedb052560c75e601cc6cd1a99c3bed573525a6
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-14 09:29:44 +0200

    Add planet detail retrieval and notFound handling in page component

--- app/planets/[name]/page.tsx
+++ app/planets/[name]/page.tsx
@@ -1,8 +1,21 @@
+import { notFound } from 'next/navigation';
+
+import planetsData from '@/data/planets.json';
+import PlanetDetail from '@/components/planet/PlanetDetail';
+
 const Page = async ({ params }: { params: Promise<{ name: string }> }) => {
   const { name } = await params;
-  console.log('name: ', name);
+  const { planets } = planetsData;
+  const planet = planets.find((planet) => planet.name.toLowerCase() === name.toLowerCase());
+  if (!planet) {
+    notFound();
+  }
 
-  return <div className='mx-auto max-w-5xl remove-scrollbar'>PLANET DETAIL</div>;
+  return (
+    <div className='mx-auto max-w-5xl remove-scrollbar'>
+      <PlanetDetail planet={planet} />
+    </div>
+  );
 };
 
 export default Page;
