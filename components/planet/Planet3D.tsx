f8dc4dd89b4ece0715f71465296177e3d1370e7b
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-18 18:40:03 +0200

    Refactor Planet3D component to support modal view and enhance lighting setup

--- components/planet/Planet3D.tsx
+++ components/planet/Planet3D.tsx
@@ -3,39 +3,54 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { IPlanet } from '@/types/planet';
 import PlanetMesh from './PlanetMesh';
-import StarryBackground from '../space/StarryBackground';
+import SceneStars from '../space/SceneStars';
 
-const Planet3D = ({ planet, onView }: { planet: IPlanet; onView?: () => void }) => {
+const Planet3D = ({
+  planet,
+  onView,
+  isModal,
+}: {
+  planet: IPlanet;
+  onView?: () => void;
+  isModal?: boolean;
+}) => {
   return (
-    <Canvas shadows className='canvas' onClick={onView}>
+    <Canvas shadows className='canvas' onClick={onView} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={'loading'}>
-        {onView && <StarryBackground />}
-        <PlanetMesh planet={planet} />
+        {onView && (
+          <>
+            <SceneStars />
+            <SceneStars reverse />
+          </>
+        )}
+        <PlanetMesh planet={planet} isModal={isModal} />
+
+        <OrbitControls
+          autoRotate
+          autoRotateSpeed={planet.rotationSpeed}
+          enablePan
+          enableZoom
+          minPolarAngle={Math.PI / 2.1}
+          maxPolarAngle={Math.PI / 2.1}
+        />
+
+        {/* Основне сонячне світло з помірною інтенсивністю */}
+        <directionalLight
+          intensity={2.0}
+          position={[5, 0, 0]}
+          castShadow
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
+        />
+
+        {/* AmbientLight – заповнює всю сцену, роблячи темну сторону не занадто темною */}
+        <ambientLight intensity={0.5} />
+
+        {/* HemisphereLight для м'якого градієнтного освітлення */}
+        <hemisphereLight args={['#ffffff', '#222222', 0.7]} position={[0, 20, 0]} />
+
+        <pointLight position={[0, 0, 0]} intensity={1.5} decay={2} distance={50} />
       </Suspense>
-
-      <OrbitControls
-        autoRotate
-        autoRotateSpeed={planet.rotationSpeed}
-        enablePan={false}
-        enableZoom={true}
-        minPolarAngle={Math.PI / 2.1}
-        maxPolarAngle={Math.PI / 2.1}
-      />
-
-      {/* Основне сонячне світло з помірною інтенсивністю */}
-      <directionalLight
-        intensity={2.0}
-        position={[10, 10, 10]}
-        castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-      />
-
-      {/* AmbientLight – заповнює всю сцену, роблячи темну сторону не занадто темною */}
-      <ambientLight intensity={0.8} />
-
-      {/* HemisphereLight для м'якого градієнтного освітлення */}
-      <hemisphereLight args={['#ffffff', '#222222', 0.7]} position={[0, 20, 0]} />
     </Canvas>
   );
 };
