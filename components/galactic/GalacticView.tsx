39806378b947d581b787a388e6122d488f6a8520
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-18 18:21:18 +0200

    Refactor GalacticView component to enhance 3D rendering and orbit controls

--- components/galactic/GalacticView.tsx
+++ components/galactic/GalacticView.tsx
@@ -1,52 +1,75 @@
 'use client';
 
 import React, { Suspense } from 'react';
+import { OrbitControls, Preload } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+
 import { IPlanet } from '@/types/planet';
 
-import StarryBackground from '../space/StarryBackground';
 import CelestialBody from './CelestialBody';
+import Sun from '../space/Sun';
+import OrbitLine from '../space/OrbitLine';
+import { useLayoutNavigationStore } from '@/store/layoutNavigationStore';
 
-const GalacticView = ({ planets }: { planets: IPlanet[] }) => {
+interface GalacticViewProps {
+  planets: IPlanet[];
+}
+
+const GalacticView: React.FC<GalacticViewProps> = ({ planets }) => {
+  // Глобальний стан для обраної планети (для OrbitControls)
+  const { followedPlanet } = useLayoutNavigationStore();
+
   return (
-    <div className='relative w-screen h-screen'>
-      {/* Three.js Canvas для фону та 3D-об'єктів */}
+    <div className='relative inset-0 w-screen h-screen'>
       <Canvas
-        className='fixed inset-0 w-full h-full -z-10'
-        camera={{ position: [0, 0, 10], fov: 75 }}
+        shadows
+        className='fixed inset-0 w-full h-full'
+        camera={{
+          fov: 60, // Кут огляду камери (Field of View) – 60 градусів
+          position: [-100, 0, 100], // Початкова позиція камери в 3D-просторі
+        }}
+        dpr={[1, 2]} // Налаштування Device Pixel Ratio: підтримка масштабування від 1 до 2 (для різних дисплеїв, зокрема Retina)
         gl={{
-          antialias: false,
-          alpha: true, // Прозорість, щоб уникнути проблем на мобільних
+          antialias: false, // Вимикаємо антиаліасинг для потенційного покращення продуктивності
+          preserveDrawingBuffer: true, // Зберігаємо буфер малювання, що може бути корисним для збереження скріншотів або пост-обробки
         }}
       >
-        <Suspense fallback={'loading'}>
-          {/* Фон із зірками */}
-          <StarryBackground />
+        {/* Якщо обрана планета, додаємо OrbitControls для обертання навколо неї */}
+        {followedPlanet && (
+          <OrbitControls
+            autoRotate
+            autoRotateSpeed={0.05}
+            enablePan={true}
+            enableZoom={true}
+            minPolarAngle={Math.PI / 2.1}
+            maxPolarAngle={Math.PI / 2.1}
+          />
+        )}
+
+        <Suspense fallback='loading'>
+          <Sun />
+          {planets.map((planet) => (
+            <group key={planet.name}>
+              <OrbitLine radius={planet.orbitRadius} />
+              <CelestialBody key={planet.name} planet={planet} />
+            </group>
+          ))}
+
+          <ambientLight intensity={0.3} />
+          <directionalLight castShadow position={[0, 0, -10]} />
+          <pointLight position={[0, 0, 0]} intensity={2} />
+          <OrbitControls
+            enablePan
+            enableZoom
+            enableRotate
+            minDistance={80}
+            maxDistance={800}
+            autoRotate
+            autoRotateSpeed={0.5}
+          />
+          <Preload all />
         </Suspense>
-
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={0.3}
-          enablePan={true}
-          enableZoom={false}
-          minPolarAngle={0}
-          maxPolarAngle={Math.PI}
-        />
       </Canvas>
-
-      {/* Окремий шар для HTML (інтерфейсного) контенту */}
-      <ul
-        className='remove-scrollbar absolute z-50 inset-0 mx-auto my-10 max-w-5xl grid gap-3 grid-cols-1 md:grid-cols-2 py-4 px-2 overflow-scroll'
-        aria-label='Planets Gallery'
-      >
-        {planets.map((planet) => (
-          <li key={planet.name} className='p-4 rounded-full w-full  pointer-events-none'>
-            {' '}
-            <CelestialBody planet={planet} />
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
