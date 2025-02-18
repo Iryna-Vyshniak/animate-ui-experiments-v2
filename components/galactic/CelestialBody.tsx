19774b2cc44cffd84ded802809c00035d2473c5e
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-18 18:45:50 +0200

    Refactor CelestialBody component to integrate PlanetMesh and enhance hover display with HTML overlay

--- components/galactic/CelestialBody.tsx
+++ components/galactic/CelestialBody.tsx
@@ -1,49 +1,34 @@
 'use client';
 
+import React from 'react';
+import { Html } from '@react-three/drei';
 import { motion } from 'framer-motion';
-import { useLayoutNavigationController } from '@/hooks/useLayoutNavigationController';
 import { IPlanet } from '@/types/planet';
-import Planet3D from '../planet/Planet3D';
+import PlanetMesh from '../planet/PlanetMesh';
+import { useLayoutNavigationStore } from '@/store/layoutNavigationStore';
 
-type CelestialBodyProps = {
+interface CelestialBodyProps {
   planet: IPlanet;
-};
+}
 
-const CelestialBody = ({ planet }: CelestialBodyProps) => {
-  const { push } = useLayoutNavigationController();
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
-    if (e.key === 'Enter' || e.key === ' ') {
-      push(planet.name);
-    }
-  };
-
+const CelestialBody: React.FC<CelestialBodyProps> = ({ planet }) => {
+  const { hoveredPlanet } = useLayoutNavigationStore();
   return (
-    <motion.article
-      layoutId={`planet-${planet.name}`}
-      className='cursor-pointer w-full h-full'
-      onClick={() => push(planet.name)}
-      tabIndex={0}
-      role='button'
-      aria-label={`View details for ${planet.name}`}
-      onKeyDown={handleKeyDown}
-    >
-      <motion.div
-        layoutId={`planet-image-${planet.name}`}
-        transition={{ type: 'spring' }}
-        className='p-4 relative overflow-hidden group flex items-center justify-center'
-      >
-        <Planet3D planet={planet} />
-        <motion.div className='absolute bottom-0 inset-0 bg-gradient-to-b from-transparent to-black opacity-0 group-hover:opacity-100 transition-all duration-350 flex justify-center rounded-full p-4'>
-          <motion.h2
-            layoutId={`planet-title-${planet.name}`}
-            className='opacity-0 group-hover:opacity-100 text-xl font-bold absolute bottom-3 group-hover:text-4xl transition-all duration-350 delay-100'
-          >
-            {planet.name}
-          </motion.h2>
-        </motion.div>
-      </motion.div>
-    </motion.article>
+    <group>
+      <PlanetMesh planet={planet} />
+      {hoveredPlanet === planet.name && (
+        <Html center pointerEvents='none'>
+          <motion.div className='relative flex justify-center rounded-full p-4'>
+            <motion.h2
+              layoutId={`planet-title-${hoveredPlanet}`}
+              className='text-base text-white/60 font-bold'
+            >
+              {hoveredPlanet}
+            </motion.h2>
+          </motion.div>
+        </Html>
+      )}
+    </group>
   );
 };
 
