8a9082c2cf11bbafe8e1a2e43e6252dd1094bfa6
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-19 23:34:41 +0200

    Refactor PlanetDetail component to integrate @react-spring for parallax scrolling and enhance visual presentation

--- components/planet/PlanetDetail.tsx
+++ components/planet/PlanetDetail.tsx
@@ -1,20 +1,20 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import { useState, useEffect, useRef } from 'react';
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
 
-import { IPlanet } from '@/types/planet';
 import PlanetImage from './PlanetImage';
 import PlanetHeader from './PlanetHeader';
 import PlanetDescription from './PlanetDescription';
 import PlanetPhysicalProperties from './PlanetPhysicalProperties';
 
 import { useLayoutNavigationController } from '@/hooks/useLayoutNavigationController';
+import { IPlanet } from '@/types/planet';
 
 const PlanetDetail = ({ planet, isModal }: { planet: IPlanet; isModal: boolean }) => {
   const { shouldCloseModal, close } = useLayoutNavigationController();
   const [mouseOffset, setMouseOffset] = useState({ x: 0, y: 0 });
-
+  const parallax = useRef<IParallax>(null!);
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
       setMouseOffset({
@@ -28,19 +28,31 @@ const PlanetDetail = ({ planet, isModal }: { planet: IPlanet; isModal: boolean }
   }, []);
 
   return (
-    <motion.article
-      className='w-full h-screen overflow-x-hidden flex flex-col items-center relative p-4 remove-scrollbar'
-      layoutId={`planet-${planet.name}`}
-    >
-      <PlanetImage planet={planet} mouseOffset={mouseOffset} isModal={isModal} />
-      <div className='flex flex-col z-10 w-full h-full p-4 mt-96 absolute bottom-0 inset-0 bg-gradient-to-b from-transparent via-[#1f0049]/90 to-slate-950 opacity-100 justify-end pointer-events-none'>
+    <Parallax ref={parallax} pages={2} className='relative z-20 remove-scrollbar '>
+      <ParallaxLayer
+        sticky={{ start: 0, end: 0 }}
+        factor={1}
+        offset={0}
+        speed={0}
+        className='sticky inset-0 flex justify-center items-center z-10 pointer-events-auto'
+      >
+        <PlanetImage planet={planet} mouseOffset={mouseOffset} isModal={isModal} />
+      </ParallaxLayer>
+
+      {/* 🔹 Інші деталі з’являються поступово при скролі */}
+      <ParallaxLayer
+        sticky={{ start: 0.5, end: 1.5 }}
+        offset={1.5}
+        speed={0.8}
+        className=' z-30 w-full h-full flex flex-col items-center justify-center p-4 bg-gradient-to-b from-transparent via-[#1f0049]/90 to-transparent pointer-events-none'
+      >
         <PlanetHeader name={planet.name} shouldClose={shouldCloseModal} onClose={close} />
-        <main className='flex flex-col gap-4  justify-between items-center px-3'>
+        <main className='relative z-50 flex flex-col gap-4 justify-between items-center px-3'>
           <PlanetDescription description={planet.description} />
           <PlanetPhysicalProperties physicalProperties={planet.physicalProperties} />
         </main>
-      </div>
-    </motion.article>
+      </ParallaxLayer>
+    </Parallax>
   );
 };
 
