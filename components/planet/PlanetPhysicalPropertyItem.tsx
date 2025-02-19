01b2dac01888f8d7820624857f938e26f078a980
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-19 23:21:51 +0200

    Enhance styling of PlanetPhysicalPropertyItem for improved visual clarity and layout

--- components/planet/PlanetPhysicalPropertyItem.tsx
+++ components/planet/PlanetPhysicalPropertyItem.tsx
@@ -5,9 +5,11 @@ type PhysicalPropertyItemProps = {
 
 const PlanetPhysicalPropertyItem = ({ title, value }: PhysicalPropertyItemProps) => {
   return (
-    <div className='flex flex-col gap-2 border border-foreground/5 rounded-md p-2 min-h-[4rem] w-48'>
-      <h3 className='font-space-mono text-sm font-bold flex-auto'>{title}</h3>
-      <p className='font-inter text-sm text-foreground/70'>{value}</p>
+    <div className='border border-foreground/5 rounded-md p-2 min-h-[4rem] w-48 bg-slate-500/10 backdrop-blur-md'>
+      <div className='flex flex-col gap-2'>
+        <h3 className='font-space-mono text-sm font-bold flex-auto'>{title}</h3>
+        <p className='font-inter text-sm text-foreground/70'>{value}</p>
+      </div>
     </div>
   );
 };
