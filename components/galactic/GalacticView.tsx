7402acfd22ccf1d9a16ace0b803f45a24a7797eb
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-19 23:37:58 +0200

    Refactor GalacticView component to integrate @react-spring for animated transitions and enhance lighting and camera settings

--- components/galactic/GalacticView.tsx
+++ components/galactic/GalacticView.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { Suspense } from 'react';
+import { useSpring, animated } from '@react-spring/web';
 import { OrbitControls, Preload } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
@@ -10,6 +11,8 @@ import CelestialBody from './CelestialBody';
 import Sun from '../space/Sun';
 import OrbitLine from '../space/OrbitLine';
 import { useLayoutNavigationStore } from '@/store/layoutNavigationStore';
+import SceneStars from '../space/SceneStars';
+import { useLayoutNavigationController } from '@/hooks/useLayoutNavigationController';
 
 interface GalacticViewProps {
   planets: IPlanet[];
@@ -18,15 +21,22 @@ interface GalacticViewProps {
 const GalacticView: React.FC<GalacticViewProps> = ({ planets }) => {
   // Глобальний стан для обраної планети (для OrbitControls)
   const { followedPlanet } = useLayoutNavigationStore();
+  const { isOpenModal } = useLayoutNavigationController();
+
+  const galacticSpring = useSpring({
+    opacity: isOpenModal ? 0 : 1,
+    scale: isOpenModal ? 0 : 1,
+    config: { mass: 2, tension: 150, friction: 25 },
+  });
 
   return (
-    <div className='relative inset-0 w-screen h-screen'>
+    <animated.div style={galacticSpring} className='relative inset-0 w-screen h-screen'>
       <Canvas
         shadows
-        className='fixed inset-0 w-full h-full'
+        className='fixed inset-0 w-screen h-screen'
         camera={{
           fov: 60, // Кут огляду камери (Field of View) – 60 градусів
-          position: [-100, 0, 100], // Початкова позиція камери в 3D-просторі
+          position: [-80, 50, 100], // Початкова позиція камери в 3D-просторі
         }}
         dpr={[1, 2]} // Налаштування Device Pixel Ratio: підтримка масштабування від 1 до 2 (для різних дисплеїв, зокрема Retina)
         gl={{
@@ -47,16 +57,19 @@ const GalacticView: React.FC<GalacticViewProps> = ({ planets }) => {
         )}
 
         <Suspense fallback='loading'>
-          <Sun />
-          {planets.map((planet) => (
-            <group key={planet.name}>
-              <OrbitLine radius={planet.orbitRadius} />
-              <CelestialBody key={planet.name} planet={planet} />
-            </group>
-          ))}
+          <SceneStars />
+          <group rotation={[Math.PI / 4, 0, 0]}>
+            <Sun />
+            {planets.map((planet) => (
+              <group key={planet.name}>
+                <OrbitLine radius={planet.orbitRadius} />
+                <CelestialBody key={planet.name} planet={planet} />
+              </group>
+            ))}
+          </group>
 
-          <ambientLight intensity={0.3} />
-          <directionalLight castShadow position={[0, 0, -10]} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[50, 50, 50]} intensity={1.2} />
           <pointLight position={[0, 0, 0]} intensity={2} />
           <OrbitControls
             enablePan
@@ -65,12 +78,14 @@ const GalacticView: React.FC<GalacticViewProps> = ({ planets }) => {
             minDistance={80}
             maxDistance={800}
             autoRotate
-            autoRotateSpeed={0.5}
+            autoRotateSpeed={0.05}
+            minPolarAngle={Math.PI / 4} // Дозволяє більший кут нахилу
+            maxPolarAngle={Math.PI / 1.5}
           />
           <Preload all />
         </Suspense>
       </Canvas>
-    </div>
+    </animated.div>
   );
 };
 
