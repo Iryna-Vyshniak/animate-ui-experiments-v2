bef2eaefe238bbda9941cc783b6c2f7d712c9285
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-19 23:27:03 +0200

    Update layout of PlanetPhysicalProperties for improved responsiveness and visual organization

--- components/planet/PlanetPhysicalProperties.tsx
+++ components/planet/PlanetPhysicalProperties.tsx
@@ -7,10 +7,10 @@ type PlanetPhysicalPropertiesProps = {
 
 const PlanetPhysicalProperties = ({ physicalProperties }: PlanetPhysicalPropertiesProps) => (
   <section
-    className='w-full md:w-1/3 flex flex-col gap-2 items-center md:items-end md:justify-center'
+    className='w-full flex flex-col gap-2 items-center justify-center'
     aria-label='Physical properties'
   >
-    <ul className='grid grid-cols-2 sm:grid-cols-3 gap-2 auto-rows-[1fr]'>
+    <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 auto-rows-[1fr]'>
       {physicalProperties.map((property, index) => (
         <li key={index} className='flex '>
           <PlanetPhysicalPropertyItem title={property.label} value={property.value} />
