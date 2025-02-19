270328c8d06b758777344e0e22f8dde108590f06
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-19 23:33:09 +0200

    Refactor CelestialBody component to integrate @react-spring for scale animations and improve interaction handling

--- components/galactic/CelestialBody.tsx
+++ components/galactic/CelestialBody.tsx
@@ -2,10 +2,12 @@
 
 import React from 'react';
 import { Html } from '@react-three/drei';
-import { motion } from 'framer-motion';
+import { useSpring, animated } from '@react-spring/three';
+
 import { IPlanet } from '@/types/planet';
 import PlanetMesh from '../planet/PlanetMesh';
 import { useLayoutNavigationStore } from '@/store/layoutNavigationStore';
+import { useLayoutNavigationController } from '@/hooks/useLayoutNavigationController';
 
 interface CelestialBodyProps {
   planet: IPlanet;
@@ -13,22 +15,27 @@ interface CelestialBodyProps {
 
 const CelestialBody: React.FC<CelestialBodyProps> = ({ planet }) => {
   const { hoveredPlanet } = useLayoutNavigationStore();
+  const { push } = useLayoutNavigationController();
+
+  // Анімація масштабу планети
+  const scaleSpring = useSpring({
+    scale: hoveredPlanet === planet.name ? 1.05 : 1,
+    config: { mass: 2, tension: 150, friction: 25 },
+  });
+
   return (
-    <group>
+    <animated.group scale={scaleSpring.scale} onClick={() => push(planet.name)}>
       <PlanetMesh planet={planet} />
       {hoveredPlanet === planet.name && (
-        <Html center pointerEvents='none'>
-          <motion.div className='relative flex justify-center rounded-full p-4'>
-            <motion.h2
-              layoutId={`planet-title-${hoveredPlanet}`}
-              className='text-base text-white/60 font-bold'
-            >
+        <Html center pointerEvents='auto'>
+          <div className='relative flex justify-center rounded-full p-4'>
+            <h2 className='absolute z-10 text-base text-white font-bold drop-shadow-[0px_2px_5px_rgba(8,0,36,1)'>
               {hoveredPlanet}
-            </motion.h2>
-          </motion.div>
+            </h2>
+          </div>
         </Html>
       )}
-    </group>
+    </animated.group>
   );
 };
 
